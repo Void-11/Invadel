33aecf6775366358e18900ba9cdee71b9023896c
Vivaan Mishra <vivaanmishra777@gmail.com>
2024-03-02 23:00:44 +0530

    Added Camera and SpringArm to Character

---
+++ Source/Invadel/Public/Character/InvadelCharacter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InvadelCharacter.generated.h"
+
+UCLASS()
+class INVADEL_API AInvadelCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	AInvadelCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
+
+public:	
+
+	
+
+};
